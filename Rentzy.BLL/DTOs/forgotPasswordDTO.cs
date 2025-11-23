ede9a6e4a311b4b3484b9e5303d7916d5a73e658
ZainabAkram268 <zainabakram268@gmail.com>
2025-11-23 14:44:07 +0500

    added functionalities related to user

---
+++ Rentzy.BLL/DTOs/forgotPasswordDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rentzy.BLL.DTOs
+{
+    public class forgotPasswordDTO
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; } = string.Empty;
+    }
+}
---
+++ Rentzy.BLL/Exceptions.cs
@@ -0,0 +1,23 @@
+﻿namespace Rentzy.BLL.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException(string message) : base(message) { }
+    }
+
+    public class BusinessException : Exception
+    {
+        public BusinessException(string message) : base(message) { }
+        public BusinessException(string message, Exception innerException) : base(message, innerException) { }
+    }
+
+    public class AuthenticationException : Exception
+    {
+        public AuthenticationException(string message) : base(message) { }
+    }
+
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message) { }
+    }
+}
