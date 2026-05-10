41eb9e705bffe59a38b06b1805a729f2d2f1f09b
izma asif <izmaasif63@gmail.com>
2026-05-11 00:11:18 +0500

    added approval service interfaces

---
+++ Rentzy.BLL/Services/ApprovalServices/IUserStatuses_service.cs
@@ -0,0 +1,19 @@
+﻿using Rentzy.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rentzy.BLL.Services.ApprovalServices
+{
+    public interface IUserStatuses_service
+    {
+        Task AddInUserStatus(int userId);
+        Task<UserStatus> GetStatusAsync(int userId);
+        Task<bool> IsBlockedAsync(int userId);
+        Task BlockUserAsync(int userId);
+        Task UnblockUserAsync(int userId);
+        Task<bool> CanEditAsync(int userId);
+    }
+}
